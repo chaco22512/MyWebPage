08936657da415c555c221578305d68953b9c7df9
Yusuke Kaga <58512479+chaco22512@users.noreply.github.com>
2020-12-07 01:57:45 +0900

    I updated the article section of my web page.
    I used JS to do the scrolling animation of each article.

--- main.js
+++ main.js
@@ -5,11 +5,28 @@ $(document).ready(function(){
     });
 
     $('.carousel').slick({
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 3,
-      nextArrow:$('.next'),
+      autoplay:true,
+      autoplaySpeed:5000,
+      dots:true,
+      slidesToShow:3,
+      slidesToScroll:3,
       prevArrow:$('.prev'),
-    });
+  });
+
+  // The animation with scrolling
+  const targetElement = document.querySelectorAll(".art");
+
+  document.addEventListener("scroll", function(){
+  for (let i=0; i<targetElement.length;i++){
+    const getElementDistance = targetElement[i].getBoundingClientRect().top
+    +targetElement[i].clientHeight*0.6;
+
+    if(window.innerHeight>getElementDistance){
+      targetElement[i].classList.add("show");
+    }
+
+  }
+
+  });
 
 })
